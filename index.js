88039a884d66ddb93138e8327965192c474cdb82
dev-talukdar <khejurbd@gmail.com>
2023-05-21 11:34:32 +0600

    vercel deploy issue is now resolved

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();  
+    // await client.connect();  
 
     const carCollection = client.db('CarSite').collection('cars');   
     const addedCollection = client.db('CarSite').collection('added');   
